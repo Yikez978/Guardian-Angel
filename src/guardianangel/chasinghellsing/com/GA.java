63adef8b80af19fba20c4c1c304daff3c9b0c67f
Sebastian Hellsing <sebastsing@gmail.com>
2016-11-14 13:50:47 -0500

    Solved folder and file creation issue.

--- src/guardianangel/chasinghellsing/com/GA.java
+++ src/guardianangel/chasinghellsing/com/GA.java
@@ -1,8 +1,10 @@
 package guardianangel.chasinghellsing.com;
 
+import java.io.IOException;
+
 public class GA {
 
-	public static void main(String[] args) {
+	public static void main(String[] args)throws IOException {
 		powerShell lock = new powerShell();
 		lock.lockedUsers();
 		// TODO Auto-generated method stub
--- src/guardianangel/chasinghellsing/com/powerShell.java
+++ src/guardianangel/chasinghellsing/com/powerShell.java
@@ -1,14 +1,17 @@
 package guardianangel.chasinghellsing.com;
 
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class powerShell {
 	public powerShell(){
 		
 	}
-	public void lockedUsers(){
+	public void lockedUsers()throws IOException{
 		File output = new File("C:/ProgramData/Guardian Angel/lockedusers.txt");
+		output.getParentFile().mkdir();
+		output.createNewFile();
 		PrintWriter out = new PrintWriter(output);
 		String command = "powershell.exe, Import-Module ActiveDirectory; Search-ADAccount -lockedout | ft samaccountname";
 	}
